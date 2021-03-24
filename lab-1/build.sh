a8bba63df28d4ae1a01a03ded583f0123ce42d5d
KuangjuX <qcx@tju.edu.cn>
2021-03-24 08:52:18 -0700

    update script

---
+++ lab-1/build.sh
@@ -0,0 +1,12 @@
+./pi_1.sh && \
+./pi_2.sh && \
+./pi_3.sh && \
+./pi_1_fuck.sh && \
+./pi_2_fuck.sh && \
+./pi_3_fuck.sh && \
+qsub pi_1.pbs && \
+qsub pi_2.pbs && \
+qsub pi_3.pbs && \
+qsub pi_1_fuck.pbs && \
+qsub pi_2_fuck.pbs && \
+qsub pi_3_fuck.pbs
