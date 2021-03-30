3bc5471c124456940f1aef25158d92fcee039614
KuangjuX <qcx@tju.edu.cn>
2021-03-30 04:28:17 -0700

    update

--- lab-1/pi_2.c
+++ lab-1/pi_2.c
@@ -11,16 +11,27 @@ long double pi;
 long long m = 0, n = 0;
 int BITS = 99999;
 
+struct position{
+    float x;
+    float y;
+};
+struct position* rands;
 
-void* handler(){
+
+void* handler(void* ID){
+    int id = (int)ID;
     int nums = N/thread_num;
+    
     // calcuate probability N times
     int local_n = 0;
     int local_m = 0;
-    srand(time(NULL));
-    for (int i = 0; i < nums; i++){
-        float x = rand() % (BITS + 1) / (float)(BITS + 1);
-        float y = rand() % (BITS + 1) / (float)(BITS + 1);
+    int start = id * nums;
+    int end = (id+1) * nums;
+
+
+    for (int i = start; i < end; i++){
+        float x = rands[i].x;
+        float y = rands[i].y;
         local_n++;
         if (x*x + y*y <= 1)local_m++;
     }
@@ -45,8 +56,20 @@ int main(int args, char* argc[]){
 
     pthread_t* threads = (pthread_t*)malloc(sizeof(pthread_t*) * thread_num);
 
+    rands = (struct position*)malloc(sizeof(struct position) *  N);
+
+    srand(time(NULL));
+    for(int i = 0; i < N; i++){
+        float x = rand() % (BITS + 1) / (float)(BITS + 1);
+        float y = rand() % (BITS + 1) / (float)(BITS + 1);
+        rands[i].x = x;
+        rands[i].y = y;
+    }
+    
+
     for (int i = 0; i < thread_num; i++){
-        pthread_create(&threads[i], NULL, handler, NULL);
+
+        pthread_create(&threads[i], NULL, handler, (void*)i);
     }
 
     for (int i = 0; i < thread_num; i++){
